0f80d5969217e483698559d56439da9580d2b95f
JPetsis <snowblind777@gmail.com>
2019-02-20 01:22:42 +0100

    5.3 corrected simple_pages_controller and user_mailer and contact form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,13 +7,17 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+  def contact_form(email, name, message)
+  @message = message
+    mail(from: email,
+         to: 'petsisjohn@gmail.com',
+         subject: "A new contact form message from #{name}")
+  end
+
   def thank_you
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-      to: 'petsisjohn@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
