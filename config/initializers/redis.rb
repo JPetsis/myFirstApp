bd9f0e8b5684311fc0e0c3b924e7fa84fcacda0c
JPetsis <snowblind777@gmail.com>
2019-04-15 20:32:17 +0200

    6.12 redis.rb fixes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   $redis = Redis.new(
     :host => ENV['REDIS_HOST'],
     :port => ENV['REDIS_PORT'],
-    :password => ENV[REDIS_PASSWORD]
+    :password => ENV['REDIS_PASSWORD']
   )
 else
   $redis = Redis.new(host: 'localhost', port: 6379)
