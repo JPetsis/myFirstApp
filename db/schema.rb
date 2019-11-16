92d31ddd1252265818a2265068d11fe34a6f7d3c
JPetsis <snowblind777@gmail.com>
2019-11-16 15:06:38 +0100

    update gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_194927) do
+ActiveRecord::Schema.define(version: 2019_06_20_123943) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
